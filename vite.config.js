5adb26cc9562200e520fdf0c4e50fa511a23d76d
anshikagupta-04 <pg8998211@gmail.com>
2025-01-11 23:27:23 +0530

    altered vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
